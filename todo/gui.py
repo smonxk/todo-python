51aa6bb3edd9e4b408da117b3ee2203bfa3636dc
Simona Dobešová <thetessie312@gmail.com>
2025-12-02 23:54:14 +0100

    add edit fn

--- todo/gui.py
+++ todo/gui.py
@@ -4,15 +4,18 @@ import FreeSimpleGUI as gui
 label = gui.Text("Type your to-do")
 input_box = gui.InputText(tooltip="Enter to-do", key="todo")
 add_button = gui.Button("Add")
+list_box = gui.Listbox(values=fns.get_todos(), key="todo_select", 
+                       enable_events=True, size=[45,10])
+edit_button = gui.Button("Edit")
 
 
 window = gui.Window("To-do app",
-                    layout=[[label], [input_box, add_button]],
+                    layout=[[label], [input_box, add_button], [list_box, edit_button]],
                     font=("Helvetica", 18))
 
 while True:
     event, values = window.read()
-    print(event)
+    print(event, "event")
     print(values)
     match(event):
         case "Add":
@@ -20,6 +23,19 @@ while True:
             new_todo = values["todo"]
             todos.append(new_todo.capitalize() + "\n")
             fns.save_todos(todos)
+            window["todo_select"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["todo_select"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = fns.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            fns.save_todos(todos)
+
+            window["todo_select"].update(values=todos) #real-time update
+        case "todo_select":
+            window["todo"].update(value=values[event][0])
         case gui.WIN_CLOSED:
             break
 
